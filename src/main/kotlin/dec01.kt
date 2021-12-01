67b4ac0a02289e301684e0948a1fe605d8155220
nickriordan <nick@anamatica.com>
2021-12-01 07:07:44 +0000

    December 1st

--- src/main/kotlin/dec01.kt
+++ src/main/kotlin/dec01.kt
@@ -1,3 +1,9 @@
 fun main() {
-    fileAsLines("dec01.txt").forEach { println(it) }
+    val depths = fileAsLines("dec01.txt").map { it.toInt() }.toList()
+
+    // Part 1
+    println(depths.windowed(2).count { it[1] > it[0] })
+
+    // Part 2
+    println(depths.windowed(3, 1).map { it.sum() }.let { a -> a.windowed(2).count { it[1] > it[0] } })
 }
