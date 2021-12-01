ef27d2497cf30123315143f8d61d817747540bbc
nickriordan <nick@anamatica.com>
2021-12-01 12:31:06 +0000

    Simplified

--- src/main/kotlin/dec01.kt
+++ src/main/kotlin/dec01.kt
@@ -5,5 +5,5 @@ fun main() {
     println(depths.windowed(2).count { it[1] > it[0] })
 
     // Part 2
-    println(depths.windowed(3, 1).map { it.sum() }.let { a -> a.windowed(2).count { it[1] > it[0] } })
+    println(depths.windowed(3, 1).map { it.sum() }.windowed(2).count { it[1] > it[0] })
 }
