4429903b5ab08e791602a4d5b2ca3a14dd284f9a
nickriordan <nick@anamatica.com>
2021-12-02 08:26:58 +0000

    Tidy up visibility

--- src/main/kotlin/dec02.kt
+++ src/main/kotlin/dec02.kt
@@ -5,14 +5,14 @@ interface Position {
     fun answer(): Int
 }
 
-data class Position1(val horz: Int = 0, val vert: Int = 0) : Position {
+data class Position1(private val horz: Int = 0, private val vert: Int = 0) : Position {
     override fun down(amt: Int) = Position1(horz, vert + amt)
     override fun up(amt: Int) = Position1(horz, vert - amt)
     override fun forward(amt: Int) = Position1(horz + amt, vert)
     override fun answer() = horz * vert
 }
 
-data class Position2(val horz: Int = 0, val vert: Int = 0, val aim: Int = 0) : Position{
+data class Position2(private val horz: Int = 0, private val vert: Int = 0, private val aim: Int = 0) : Position {
     override fun down(amt: Int) = Position2(horz, vert, aim + amt)
     override fun up(amt: Int) = Position2(horz, vert, aim - amt)
     override fun forward(amt: Int) = Position2(horz + amt, vert + (aim * amt), aim)
