136b7bffedda142cae41fa8e9cd124f79411a9b5
nickriordan <nick@anamatica.com>
2021-12-04 18:08:44 +0000

    Improved domain language dec 04

--- src/main/kotlin/dec04.kt
+++ src/main/kotlin/dec04.kt
@@ -1,35 +1,35 @@
 data class Board(private val rows: List<List<Int?>>, val winningNumber: Int = -1) {
-    fun attempt(numberToTry: Int) =
+    fun call(number: Int) =
         if (winningNumber < 0) {
-            val updatedRows = rows.map { it.map { e -> if (e == numberToTry) null else e } }
+            val updatedRows = rows.map { it.map { e -> if (e == number) null else e } }
             val cols = updatedRows[0].indices.map { ix -> updatedRows.map { it[ix] } }
             if (updatedRows.any { r -> r.all { it == null } } || cols.any { c -> c.all { it == null } }) {
-                Board(updatedRows, numberToTry)
+                Board(updatedRows, number)
             } else
                 Board(updatedRows)
         } else
             this
 
-    fun isWin() = winningNumber >= 0
+    fun isWon() = winningNumber >= 0
     fun sumOfRemaining() = rows.sumOf { it.filterNotNull().sum() }
 }
 
-data class Round(val boards: List<Board>, val lastTry: Int = 0) {
-    fun attempt(n: Int) = Round(boards.map { board -> board.attempt(n) }, n)
-    fun anyBoardWithWin() = boards.any { it.isWin() }
-    fun allBoardsWon() = boards.all { it.isWin() }
+data class Round(private val boards: List<Board>, private val lastTry: Int = 0) {
+    fun call(number: Int) = Round(boards.map { board -> board.call(number) }, number)
+    fun anyBoardsWon() = boards.any { it.isWon() }
+    fun allBoardsWon() = boards.all { it.isWon() }
     fun winningScore() = boards.first { it.winningNumber == lastTry }.sumOfRemaining() * lastTry
 }
 
 private fun calculateRounds(): List<Round> {
     val lines = loadStrings("dec04.txt")
-    val tries = lines[0].split(",").map(String::toInt)
+    val numbersToCall = lines[0].split(",").map(String::toInt)
     val boards = lines.drop(2).windowed(5, 6).map {
         it.map { l -> l.trim().split("\\s+".toRegex()).map(String::toInt) }
     }.map { Board(it) }
 
-    return tries.fold(listOf(Round(boards))) { rounds, numberToTry -> rounds + rounds.last().attempt(numberToTry) }
+    return numbersToCall.fold(listOf(Round(boards))) { rounds, number -> rounds + rounds.last().call(number) }
 }
 
-fun dec04part1() = calculateRounds().first { it.anyBoardWithWin() }.winningScore()
+fun dec04part1() = calculateRounds().first { it.anyBoardsWon() }.winningScore()
 fun dec04part2() = calculateRounds().first { it.allBoardsWon() }.winningScore()
