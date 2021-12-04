ce4f8b9b1440a12acbb06f422e64c8339518d2e1
nickriordan <nick@anamatica.com>
2021-12-04 17:35:37 +0000

    Minor improvement in clarity dec 4th

--- src/main/kotlin/dec04.kt
+++ src/main/kotlin/dec04.kt
@@ -1,10 +1,10 @@
 data class Board(private val rows: List<List<Int?>>, val winningNumber: Int = -1) {
-    fun attempt(n: Int) =
+    fun attempt(numberToTry: Int) =
         if (winningNumber < 0) {
-            val updatedRows = rows.map { it.map { e -> if (e == n) null else e } }
+            val updatedRows = rows.map { it.map { e -> if (e == numberToTry) null else e } }
             val cols = updatedRows[0].indices.map { ix -> updatedRows.map { it[ix] } }
             if (updatedRows.any { r -> r.all { it == null } } || cols.any { c -> c.all { it == null } }) {
-                Board(updatedRows, n)
+                Board(updatedRows, numberToTry)
             } else
                 Board(updatedRows)
         } else
@@ -18,7 +18,7 @@ data class Round(val boards: List<Board>, val lastTry: Int = 0) {
     fun attempt(n: Int) = Round(boards.map { board -> board.attempt(n) }, n)
     fun anyBoardWithWin() = boards.any { it.isWin() }
     fun allBoardsWon() = boards.all { it.isWin() }
-    fun boardsWonInThisRound() = boards.filter { it.winningNumber == lastTry }
+    fun winningScore() = boards.first { it.winningNumber == lastTry }.sumOfRemaining() * lastTry
 }
 
 private fun calculateRounds(): List<Round> {
@@ -28,11 +28,8 @@ private fun calculateRounds(): List<Round> {
         it.map { l -> l.trim().split("\\s+".toRegex()).map(String::toInt) }
     }.map { Board(it) }
 
-    return tries.fold(listOf(Round(boards))) { rounds, n -> rounds + rounds.last().attempt(n) }
+    return tries.fold(listOf(Round(boards))) { rounds, numberToTry -> rounds + rounds.last().attempt(numberToTry) }
 }
 
-fun dec04part1() = calculateRounds().first { it.anyBoardWithWin() }
-    .let { it.boardsWonInThisRound().first().sumOfRemaining() * it.lastTry }
-
-fun dec04part2() = calculateRounds().first { it.allBoardsWon() }
-    .let { it.boardsWonInThisRound().first().sumOfRemaining() * it.lastTry }
+fun dec04part1() = calculateRounds().first { it.anyBoardWithWin() }.winningScore()
+fun dec04part2() = calculateRounds().first { it.allBoardsWon() }.winningScore()
